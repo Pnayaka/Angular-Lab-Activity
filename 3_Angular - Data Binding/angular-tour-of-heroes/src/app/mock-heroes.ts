a741aba23626122ad3601016bd1e36fb8e05a2f3
pnayaka <pari2sn@gmail.com>
2020-06-06 21:06:14 +0530

    lab activity solutions
    Please find the lab activity solutions with regards to our Angular Training.

---
+++ 3_Angular - Data Binding/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Tiger' },
+  { id: 12, name: 'Lion' },
+  { id: 13, name: 'hippopotamus' },
+  { id: 14, name: 'King Cobra' },
+  { id: 15, name: 'Bear' },
+  { id: 16, name: 'Cheetha' },
+  { id: 17, name: 'Elephant' },
+  { id: 18, name: 'Geraphe' },
+  { id: 19, name: 'Deer' },
+  { id: 20, name: 'Leopard' }
+];
